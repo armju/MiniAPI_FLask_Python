0e00dcf5b15cb8d9f633e2f6df66109c08ab6ce3
Armando Juarez Hernandez <ajuarezhe@gmail.com>
2023-09-29 16:44:46 -0600

    API

---
+++ main.py
@@ -0,0 +1,25 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def root():
+    return "Home"
+
+
+@app.route("/users/<user_id>")
+def get_user(user_id):
+    user = {'id': user_id, 'name':'test','telefono':'999-666-333'}
+    query = request.args.get('query')
+    if query:
+        user["query"] = query
+    return jsonify(user),200
+
+
+@app.route("/users", methods = ['POST'])
+def create_user():
+    data = request.get_json()
+    return jsonify(data), 201
+
+if __name__ == "__main__":
+    app.run(debug=True)
